9372b3ac9772fccddd4f0e6a7421bbffb4cb02ef
JoeFalzone <falzonej@gmail.com>
2022-04-12 17:17:04 -0400

    add functionality for classes to be added to col(s)

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const container = document.querySelector('#container');
 const rowsContainer = document.querySelector('#rowsContainer')
-const col = document.querySelector('.col');
 
 
 
@@ -23,14 +22,31 @@ function createRow(x,y) {
 
 function createGridDivs(x, y) {
     for (let i = 0; i < x; i++){
-        createRow(x,y);
+        createRow(x,y);;
         
     }
     
 }
+
 createGridDivs(10,10)
+
+const col = document.querySelectorAll('.col');
+
+col.forEach(function(col){
+    col.addEventListener('mouseover', function() {
+        col.classList.add('touched');
+    })})
+
+//col.addEventListener('mouseenter', function() {
+//    col.classList.add('touched')
+//})
 
 //I need to add a function that will add a class of touched
 // to each div after it is moused over and that class will
 // contain the styling for changing colors and can be group
 // cleared for the reset button later
+
+//if we want to add a color changing function to the 
+//E-A-S we could set a mouseon and mouseout event listener 
+//mouseon designed to change to a certain color and mouseout
+//with a function that produces a random color
