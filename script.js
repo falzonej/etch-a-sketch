9330a94f8d92a808362af1ce1d92eee166d0e17c
JoeFalzone <falzonej@gmail.com>
2022-04-15 12:12:31 -0400

    added grid-drawing input, styling, function button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('#container');
 const rowsContainer = document.querySelector('#rowsContainer')
-const  resetButton = document.querySelector('#physicalResetButton');
-
+const  resetButton = document.querySelector('#resetButton');
+const newGridButton = document.querySelector('#newGridButton');
 
 
 function createRow(x,y) {
@@ -29,9 +29,16 @@ function createGridDivs(x, y) {
     
 }
 
+newGridButton.addEventListener('click', function(){
+  let x = prompt('Enter grid Height', '0');
+  let y = prompt('Enter grid Width', '0')
+  console.log(x)
+  console.log(y)
+  createGridDivs(x,y);
+})
 
-
-createGridDivs(100,100)
+//createGridDivs(50,50)
+//createGridDivs(100,100)
 
 const col = document.querySelectorAll('.col');
 
@@ -44,3 +51,7 @@ col.forEach(function(col){
         resetButton.addEventListener('click', function() {
             col.classList.remove('touched');
         })})
+
+//next add player input for grid size
+//then work on up and downsizing the window
+//while keeping the grid the same pixel size
