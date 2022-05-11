5abccef717525a1246dee459f9f57088eb3e9da1
JoeFalzone <falzonej@gmail.com>
2022-05-10 22:18:15 -0400

    added increasing opacity coloring to mouseover events

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const newGridButton = document.querySelector('#newGridButton');
 function createRow(gridSize,gridSize) {
     const rowContainer = document.createElement('rowContainer');
     rowContainer.className = 'rowContainer';
-    colsContainer.append(rowContainer)
+    colsContainer.append(rowContainer);
 
         function addCol() {
             const gridItem = document.createElement('gridItem');
@@ -24,14 +24,13 @@ function createRow(gridSize,gridSize) {
 function createGridDivs(gridSize, gridSize) {
     for (let i = 0; i < gridSize; i++){
         createRow(gridSize,gridSize);;
-    }   
-}
+    }}
 newGridButton.addEventListener('click', function(){
 let gridSize = prompt('Enter grid size', '0');
 if (gridSize>100){
     window.alert('That grid size is too large for this game, try again.');
   }else if(gridSize>100){
-    window.alert('That grid size is too large for this game, try again.')
+    window.alert('That grid size is too large for this game, try again.');
   }else{
   createGridDivs(gridSize,gridSize);
   }
@@ -40,13 +39,21 @@ if (gridSize>100){
   
 
   gridItem.forEach(function(gridItem){
-    color = "#"
-      gridItem.addEventListener('mouseover', function() {
-        placeHolder = "#"
-        color = placeHolder += Math.random().toString().slice(2,8);
-        gridItem.style.backgroundColor = color;
-        gridItem.style.opacity += '0.5';
-        })})
+    gridItem.addEventListener('mouseover', function() {
+      placeHolder = "#";
+      color = placeHolder += Math.random().toString().slice(2,8);
+      gridItem.style.backgroundColor = color;
+      gridItem.style.opacity += 0.1;
+      })})
+
+  gridItem.forEach(function(gridItem){
+    gridItem.addEventListener('mouseover', function() {
+      let opacity = parseFloat(gridItem.style.opacity);
+      for(; opacity < 1; opacity++){
+        let jsOpacity = opacity += .1;
+        gridItem.style.opacity = jsOpacity
+        return;}
+      })})
 
   gridItem.forEach(function(gridItem){
       resetButton.addEventListener('click', function() {
@@ -58,5 +65,13 @@ if (gridSize>100){
   gridItem.forEach(function(gridItem){
     newGridButton.addEventListener('click', function(){
       gridItem.remove();
-    })})
+      })})
 })
+
+//PLACES FOR FUTURE IMPROVEMENT
+
+//ADD BUTTONS TO CHANGE BETWEEN BLACK&WHITE BACKGROUND COLORING,
+//RAINBOW COLORING, AND TURNING FADE COLORING ON AND OFF.
+//THE BLACK&WHITE SHOULD DEACTIVATE RAINBOW AND VICE VERSA
+//BUT THE FADE COLORING SHOULD BE INDEPENDENT OF THESE TOO AND
+//STAY ACTIVE WHENEVER SET.
